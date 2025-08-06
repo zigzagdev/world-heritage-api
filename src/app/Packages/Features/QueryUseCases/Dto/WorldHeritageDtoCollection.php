fbd83d259be75f8c401de9bac1d8e2297ecf98a9
Application-drop-up <msts.oo0131@gmail.com>
2025-08-06 13:26:00 +0900

    feat: create-dto_dtocollection

--- src/app/Packages/Features/QueryUseCases/Dto/WorldHeritageDtoCollection.php
+++ src/app/Packages/Features/QueryUseCases/Dto/WorldHeritageDtoCollection.php
@@ -1,1 +1,40 @@
 <?php
+
+namespace App\Packages\Features\QueryUseCases\Dto;
+
+class WorldHeritageDtoCollection
+{
+    private array $heritages;
+
+    public function __construct(WorldHeritageDto ...$heritages)
+    {
+        $this->heritages = $heritages;
+    }
+
+    public function toArray(): array
+    {
+        return array_map(function (WorldHeritageDto $heritage) {
+            return [
+                'id' => $heritage->getId(),
+                'unescoId' => $heritage->getUnescoId(),
+                'officialName' => $heritage->getOfficialName(),
+                'name' => $heritage->getName(),
+                'country' => $heritage->getCountry(),
+                'region' => $heritage->getRegion(),
+                'category' => $heritage->getCategory(),
+                'yearInscribed' => $heritage->getYearInscribed(),
+                'latitude' => $heritage->getLatitude(),
+                'longitude' => $heritage->getLongitude(),
+                'isEndangered' => $heritage->isEndangered(),
+                'nameJp' => $heritage->getNameJp(),
+                'stateParty' => $heritage->getStateParty(),
+                'criteria' => $heritage->getCriteria(),
+                'areaHectares' => $heritage->getAreaHectares(),
+                'bufferZoneHectares' => $heritage->getBufferZoneHectares(),
+                'shortDescription' => $heritage->getShortDescription(),
+                'imageUrl' => $heritage->getImageUrl(),
+                'unescoSiteUrl' => $heritage->getUnescoSiteUrl()
+            ];
+        }, $this->heritages);
+    }
+}
--- src/app/Packages/Features/QueryUseCases/Factory/WorldHeritageDtoCollectionFactory.php
+++ src/app/Packages/Features/QueryUseCases/Factory/WorldHeritageDtoCollectionFactory.php
@@ -1,1 +1,38 @@
 <?php
+
+namespace App\Packages\Features\QueryUseCases\Factory;
+
+use App\Packages\Features\QueryUseCases\Dto\WorldHeritageDto;
+use App\Packages\Features\QueryUseCases\Dto\WorldHeritageDtoCollection;
+
+class WorldHeritageDtoCollectionFactory
+{
+    public static function build(array $data): WorldHeritageDtoCollection
+    {
+        $heritages = array_map(function ($heritage) {
+            return new WorldHeritageDto(
+                id: $heritage['id'],
+                unescoId: $heritage['unesco_id'],
+                officialName: $heritage['official_name'],
+                name: $heritage['name'],
+                country: $heritage['country'],
+                region: $heritage['region'],
+                category: $heritage['category'],
+                yearInscribed: $heritage['year_inscribed'],
+                latitude: $heritage['latitude'],
+                longitude: $heritage['longitude'],
+                isEndangered: $heritage['is_endangered'] ?? false,
+                nameJp: $heritage['name_jp'] ?? null,
+                stateParty: $heritage['state_party'] ?? null,
+                criteria: $heritage['criteria'] ?? null,
+                areaHectares: $heritage['area_hectares'] ?? null,
+                bufferZoneHectares: $heritage['buffer_zone_hectares'] ?? null,
+                shortDescription: $heritage['short_description'] ?? null,
+                imageUrl: $heritage['image_url'] ?? null,
+                unescoSiteUrl: $heritage['unesco_site_url'] ?? null
+            );
+        }, $data);
+
+        return new WorldHeritageDtoCollection(...$heritages);
+    }
+}
