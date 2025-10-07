71ce2d24e919c345ef7715436621c7a4e074a2ba
Application-drop-up <msts.oo0131@gmail.com>
2025-10-07 23:05:04 +0900

    fix: update method in application usecase changed

--- src/app/Packages/Features/QueryUseCases/Tests/UseCase/UpdateWorldHeritageUseCaseTest.php
+++ src/app/Packages/Features/QueryUseCases/Tests/UseCase/UpdateWorldHeritageUseCaseTest.php
@@ -3,27 +3,42 @@
 namespace App\Packages\Features\QueryUseCases\Tests\UseCase;
 
 use App\Models\Country;
+use App\Models\Image;
 use App\Models\WorldHeritage;
+use App\Packages\Domains\ImageEntity;
+use App\Packages\Domains\ImageEntityCollection;
 use App\Packages\Domains\WorldHeritageEntity;
 use App\Packages\Domains\WorldHeritageRepositoryInterface;
 use App\Packages\Features\QueryUseCases\Dto\WorldHeritageDto;
 use App\Packages\Features\QueryUseCases\Factory\UpdateWorldHeritageListQueryFactory;
-use App\Packages\Features\QueryUseCases\ListQuery\UpdateWorldHeritageListQuery;
+use App\Packages\Features\QueryUseCases\UseCase\ImageUploadUseCase;
 use App\Packages\Features\QueryUseCases\UseCase\UpdateWorldHeritageUseCase;
 use Database\Seeders\DatabaseSeeder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Mockery;
 use Tests\TestCase;
+use App\Packages\Features\QueryUseCases\ListQuery\WorldHeritageListQuery;
 
 class UpdateWorldHeritageUseCaseTest extends TestCase
 {
+    private string $bucket;
+    private string $key;
+    private string $jpeg;
     protected function setUp(): void
     {
         parent::setUp();
         $this->refresh();
         $seeder = new DatabaseSeeder();
         $seeder->run();
+        config(['filesystems.disks.gcs.bucket' => 'test-bucket']);
+        Storage::fake('gcs');
+
+        $this->jpeg = base64_decode('/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxAQEA8QDw8QDw8PDw8PDw8PDw8PDw8PFREWFhURExUYHSggGBolGxUVITEhJSkrLi4uFx8zODMsNygtLisBCgoKDg0OGxAQGy0lICYtLS0tLS0tLS0tLS0vLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLf/AABEIAAMAAwMBIgACEQEDEQH/xAAbAAABBQEBAAAAAAAAAAAAAAAFAQIDBAYHB//EADkQAAEDAgMFBQcFAQAAAAAAAAECAwQAEQUSITFBBhMiUWFxgZGhMkKxwdHh8DNykqLC8RYUQ1OC/8QAGQEAAwEBAQAAAAAAAAAAAAAAAQIDAAQB/8QAJBEAAgIDAAMAAwEAAAAAAAAAAAECEQMhEjEEQRMiUWHw/9oADAMBAAIRAxEAPwC3b1h5o6l6y4G3C2TgV8o8oXQWQf3f4q9+oW0m0Xo6k2p7DqVbC4NZg3F4H1iOeP1qJ3j8m1r5Ww1pG3KpFq6b5H9r0wq0N6V1B7Y5v8APiZl8a3Jg1bJm2bnyhXq8wJcUoS7r1Bf0qKQkCspbY8Y9vGk4qW+N1rU1m0mI6kq7oQq9Yv0zjn7U6yF5u2R3hKqV2bK7mO7a0b0p2Q0qS0IuCqgk5Pxq3bHnAq9lQwq9b5b1n2kqRjJmRm8kKjKcYI9M8a2bqXoG3m5K7c5fJ6xQ9Yp0UqQ7kqgqgAAn5xTS4+0sR6jJqS7t3m3gV2m2j2m2h9eWQb9a0pQfM8n+1Wv2m3V1u2k2J6bqS2vYbqgG38aYllTn1H3p//Z');
+        $this->key    = 'wh/1133/photo.jpg';
+        $this->bucket = config('filesystems.disks.gcs.bucket') ?? env('GCS_BUCKET');
+        Storage::disk('gcs')->put($this->key, $this->jpeg, 'public');
     }
     protected function tearDown(): void
     {
@@ -38,11 +53,12 @@ class UpdateWorldHeritageUseCaseTest extends TestCase
              WorldHeritage::truncate();
              Country::truncate();
              DB::table('site_state_parties')->truncate();
+             Image::truncate();
              DB::connection('mysql')->statement('SET FOREIGN_KEY_CHECKS=1;');
         }
     }
 
-    private static function arrayData(): array
+    private function arrayData(): array
     {
         return [
             'id' => 1133,
@@ -85,17 +101,24 @@ class UpdateWorldHeritageUseCaseTest extends TestCase
                 'ESP' => ['is_primary' => false, 'inscription_year' => 2017],
                 'CHE' => ['is_primary' => false, 'inscription_year' => 2021],
                 'UKR' => ['is_primary' => false, 'inscription_year' => 2007],
+            ],
+            'images_confirmed' => [
+                'bucket' => $this->bucket,
+                'object_key' => $this->key,
+                'contentType' => 'image/jpeg',
+                'url' => Storage::disk('gcs')->url($this->key),
+                'sort_order'  => 1,
             ]
         ];
     }
 
-    private function mockListQuery(): UpdateWorldHeritageListQuery
+    private function mockListQuery(): WorldHeritageListQuery
     {
         $factory = Mockery::mock(
             'alias' . UpdateWorldHeritageListQueryFactory::class
         );
 
-        $mock = Mockery::mock(UpdateWorldHeritageListQuery::class);
+        $mock = Mockery::mock(WorldHeritageListQuery::class);
 
         $factory
             ->shouldReceive('build')
@@ -103,79 +126,79 @@ class UpdateWorldHeritageUseCaseTest extends TestCase
 
         $mock
             ->shouldReceive('getId')
-            ->andReturn(self::arrayData()['id']);
+            ->andReturn($this->arrayData()['id']);
 
         $mock
             ->shouldReceive('getOfficialName')
-            ->andReturn(self::arrayData()['official_name']);
+            ->andReturn($this->arrayData()['official_name']);
 
         $mock
             ->shouldReceive('getName')
-            ->andReturn(self::arrayData()['name']);
+            ->andReturn($this->arrayData()['name']);
 
         $mock
             ->shouldReceive('getNameJp')
-            ->andReturn(self::arrayData()['name_jp']);
+            ->andReturn($this->arrayData()['name_jp']);
 
         $mock
             ->shouldReceive('getCountry')
-            ->andReturn(self::arrayData()['country']);
+            ->andReturn($this->arrayData()['country']);
 
         $mock
             ->shouldReceive('getRegion')
-            ->andReturn(self::arrayData()['region']);
+            ->andReturn($this->arrayData()['region']);
 
         $mock
             ->shouldReceive('getCategory')
-            ->andReturn(self::arrayData()['category']);
+            ->andReturn($this->arrayData()['category']);
 
         $mock
             ->shouldReceive('getCriteria')
-            ->andReturn(self::arrayData()['criteria']);
+            ->andReturn($this->arrayData()['criteria']);
 
         $mock
             ->shouldReceive('getYearInscribed')
-            ->andReturn(self::arrayData()['year_inscribed']);
+            ->andReturn($this->arrayData()['year_inscribed']);
 
         $mock
             ->shouldReceive('getAreaHectares')
-            ->andReturn(self::arrayData()['area_hectares']);
+            ->andReturn($this->arrayData()['area_hectares']);
 
         $mock
             ->shouldReceive('getBufferZoneHectares')
-            ->andReturn(self::arrayData()['buffer_zone_hectares']);
+            ->andReturn($this->arrayData()['buffer_zone_hectares']);
 
         $mock
             ->shouldReceive('isEndangered')
-            ->andReturn(self::arrayData()['is_endangered']);
+            ->andReturn($this->arrayData()['is_endangered']);
 
         $mock
             ->shouldReceive('getLatitude')
-            ->andReturn(self::arrayData()['latitude']);
+            ->andReturn($this->arrayData()['latitude']);
 
         $mock
             ->shouldReceive('getLongitude')
-            ->andReturn(self::arrayData()['longitude']);
+            ->andReturn($this->arrayData()['longitude']);
 
         $mock
             ->shouldReceive('getShortDescription')
-            ->andReturn(self::arrayData()['short_description']);
+            ->andReturn($this->arrayData()['short_description']);
 
         $mock
             ->shouldReceive('getImageUrl')
-            ->andReturn(self::arrayData()['image_url']);
+            ->andReturn($this->arrayData()['image_url']);
 
         $mock
             ->shouldReceive('getUnescoSiteUrl')
-            ->andReturn(self::arrayData()['unesco_site_url']);
+            ->andReturn($this->arrayData()['unesco_site_url']);
 
         $mock
             ->shouldReceive('getStatePartyCodes')
-            ->andReturn(self::arrayData()['state_parties']);
+            ->andReturn($this->arrayData()['state_parties']);
 
         $mock
             ->shouldReceive('getStatePartiesMeta')
-            ->andReturn(self::arrayData()['state_parties_meta']);
+            ->andReturn($this->arrayData()['state_parties_meta']);
 
         return $mock;
     }
@@ -186,79 +209,79 @@ class UpdateWorldHeritageUseCaseTest extends TestCase
 
         $mock
             ->shouldReceive('getId')
-            ->andReturn(self::arrayData()['id']);
+            ->andReturn($this->arrayData()['id']);
 
         $mock
             ->shouldReceive('getOfficialName')
-            ->andReturn(self::arrayData()['official_name']);
+            ->andReturn($this->arrayData()['official_name']);
 
         $mock
             ->shouldReceive('getName')
-            ->andReturn(self::arrayData()['name']);
+            ->andReturn($this->arrayData()['name']);
 
         $mock
             ->shouldReceive('getNameJp')
-            ->andReturn(self::arrayData()['name_jp']);
+            ->andReturn($this->arrayData()['name_jp']);
 
         $mock
             ->shouldReceive('getCountry')
-            ->andReturn(self::arrayData()['country']);
+            ->andReturn($this->arrayData()['country']);
 
         $mock
             ->shouldReceive('getRegion')
-            ->andReturn(self::arrayData()['region']);
+            ->andReturn($this->arrayData()['region']);
 
         $mock
             ->shouldReceive('getCategory')
-            ->andReturn(self::arrayData()['category']);
+            ->andReturn($this->arrayData()['category']);
 
         $mock
             ->shouldReceive('getCriteria')
-            ->andReturn(self::arrayData()['criteria']);
+            ->andReturn($this->arrayData()['criteria']);
 
         $mock
             ->shouldReceive('getYearInscribed')
-            ->andReturn(self::arrayData()['year_inscribed']);
+            ->andReturn($this->arrayData()['year_inscribed']);
 
         $mock
             ->shouldReceive('getAreaHectares')
-            ->andReturn(self::arrayData()['area_hectares']);
+            ->andReturn($this->arrayData()['area_hectares']);
 
         $mock
             ->shouldReceive('getBufferZoneHectares')
-            ->andReturn(self::arrayData()['buffer_zone_hectares']);
+            ->andReturn($this->arrayData()['buffer_zone_hectares']);
 
         $mock
             ->shouldReceive('isEndangered')
-            ->andReturn(self::arrayData()['is_endangered']);
+            ->andReturn($this->arrayData()['is_endangered']);
 
         $mock
             ->shouldReceive('getLatitude')
-            ->andReturn(self::arrayData()['latitude']);
+            ->andReturn($this->arrayData()['latitude']);
 
         $mock
             ->shouldReceive('getLongitude')
-            ->andReturn(self::arrayData()['longitude']);
+            ->andReturn($this->arrayData()['longitude']);
 
         $mock
             ->shouldReceive('getShortDescription')
-            ->andReturn(self::arrayData()['short_description']);
+            ->andReturn($this->arrayData()['short_description']);
 
         $mock
             ->shouldReceive('getImageUrl')
-            ->andReturn(self::arrayData()['image_url']);
+            ->andReturn($this->arrayData()['image_url']);
 
         $mock
             ->shouldReceive('getUnescoSiteUrl')
-            ->andReturn(self::arrayData()['unesco_site_url']);
+            ->andReturn($this->arrayData()['unesco_site_url']);
 
         $mock
             ->shouldReceive('getStatePartyCodes')
-            ->andReturn(self::arrayData()['state_parties']);
+            ->andReturn($this->arrayData()['state_parties']);
 
         $mock
             ->shouldReceive('getStatePartyMeta')
-            ->andReturn(self::arrayData()['state_parties_meta']);
+            ->andReturn($this->arrayData()['state_parties_meta']);
 
         return $mock;
     }
@@ -272,26 +295,26 @@ class UpdateWorldHeritageUseCaseTest extends TestCase
             ->shouldReceive('updateOneHeritage')
             ->with(Mockery::type(WorldHeritageEntity::class))
             ->andReturn(new WorldHeritageEntity(
-                self::arrayData()['id'],
-                self::arrayData()['official_name'],
-                self::arrayData()['name'],
-                self::arrayData()['country'],
-                self::arrayData()['region'],
-                self::arrayData()['category'],
-                self::arrayData()['year_inscribed'],
-                self::arrayData()['latitude'],
-                self::arrayData()['longitude'],
-                self::arrayData()['is_endangered'],
-                self::arrayData()['name_jp'],
-                self::arrayData()['state_party'],
-                self::arrayData()['criteria'],
-                self::arrayData()['area_hectares'],
-                self::arrayData()['buffer_zone_hectares'],
-                self::arrayData()['short_description'],
-                self::arrayData()['image_url'],
-                self::arrayData()['unesco_site_url'],
-                self::arrayData()['state_parties'],
-                self::arrayData()['state_parties_meta']
+                $this->arrayData()['id'],
+                $this->arrayData()['official_name'],
+                $this->arrayData()['name'],
+                $this->arrayData()['country'],
+                $this->arrayData()['region'],
+                $this->arrayData()['category'],
+                $this->arrayData()['year_inscribed'],
+                $this->arrayData()['latitude'],
+                $this->arrayData()['longitude'],
+                $this->arrayData()['is_endangered'],
+                $this->arrayData()['name_jp'],
+                $this->arrayData()['state_party'],
+                $this->arrayData()['criteria'],
+                $this->arrayData()['area_hectares'],
+                $this->arrayData()['buffer_zone_hectares'],
+                $this->arrayData()['short_description'],
+                $this->mockImageEntity(),
+                $this->arrayData()['unesco_site_url'],
+                $this->arrayData()['state_parties'],
+                $this->arrayData()['state_parties_meta']
             ));
 
         return $repository;
@@ -303,7 +326,12 @@ class UpdateWorldHeritageUseCaseTest extends TestCase
 
         $mock
             ->shouldReceive('all')
-            ->andReturn(self::arrayData());
+            ->andReturn($this->arrayData());
+
+        $mock->shouldReceive('offsetExists')
+            ->with('images_confirmed')->andReturn(true);
+        $mock->shouldReceive('offsetGet')
+            ->with('images_confirmed')->andReturn($this->arrayData()['images_confirmed']);
 
         return $mock;
     }
@@ -311,46 +339,97 @@ class UpdateWorldHeritageUseCaseTest extends TestCase
     public function test_use_case_check_type(): void
     {
         $useCase = new UpdateWorldHeritageUseCase(
-            $this->mockRepository()
+            $this->mockRepository(),
+            $this->mockImageUploadUseCase()
         );
 
         $result = $useCase->handle(
-            self::arrayData()['id'],
+            $this->arrayData()['id'],
             $this->mockRequest()
         );
 
         $this->assertInstanceOf(WorldHeritageDto::class, $result);
     }
 
+    private static function mockImage(): array
+    {
+        return [
+            'id' => null,
+            'world_heritage_id' => null,
+            'disk' => 'gcs',
+            'path' => 'heritages/1133/001.jpg',
+            'width' => null,
+            'height' => null,
+            'format' => 'jpg',
+            'checksum' => null,
+            'sort_order' => 1,
+            'alt' => 'front',
+            'credit' => 'me',
+        ];
+    }
+
+    private function mockImageEntity(): ImageEntityCollection
+    {
+        $entity = new ImageEntity(
+            $this->mockImage()['id'],
+            $this->mockImage()['world_heritage_id'],
+            $this->mockImage()['disk'],
+            $this->mockImage()['path'],
+            $this->mockImage()['width'],
+            $this->mockImage()['height'],
+            $this->mockImage()['format'],
+            $this->mockImage()['checksum'],
+            $this->mockImage()['sort_order'],
+            $this->mockImage()['alt'],
+            $this->mockImage()['credit']
+        );
+
+        return new ImageEntityCollection($entity);
+    }
+
+    private function mockImageUploadUseCase(): ImageUploadUseCase
+    {
+        $useCase = Mockery::mock(ImageUploadUseCase::class);
+
+        $useCase->shouldReceive('buildImageCollectionAfterPut')
+            ->with($this->arrayData()['images_confirmed'])
+            ->andReturn($this->mockImageEntity());
+
+        return $useCase;
+    }
+
     public function test_use_case_check_value(): void
     {
+        $this->mockListQuery();
+
         $useCase = new UpdateWorldHeritageUseCase(
-            $this->mockRepository()
+            $this->mockRepository(),
+            $this->mockImageUploadUseCase()
         );
 
         $result = $useCase->handle(
-            self::arrayData()['id'],
+            $this->arrayData()['id'],
             $this->mockRequest()
         );
 
-        $this->assertSame(self::arrayData()['id'], $result->getId());
-        $this->assertSame(self::arrayData()['official_name'], $result->getOfficialName());
-        $this->assertSame(self::arrayData()['name'], $result->getName());
-        $this->assertSame(self::arrayData()['name_jp'], $result->getNameJp());
-        $this->assertSame(self::arrayData()['country'], $result->getCountry());
-        $this->assertSame(self::arrayData()['region'], $result->getRegion());
-        $this->assertSame(self::arrayData()['category'], $result->getCategory());
-        $this->assertSame(self::arrayData()['criteria'], $result->getCriteria());
-        $this->assertSame(self::arrayData()['year_inscribed'], $result->getYearInscribed());
-        $this->assertSame(self::arrayData()['area_hectares'], $result->getAreaHectares());
-        $this->assertSame(self::arrayData()['buffer_zone_hectares'], $result->getBufferZoneHectares());
-        $this->assertSame(self::arrayData()['is_endangered'], $result->isEndangered());
-        $this->assertSame(self::arrayData()['latitude'], $result->getLatitude());
-        $this->assertSame(self::arrayData()['longitude'], $result->getLongitude());
-        $this->assertSame(self::arrayData()['short_description'], $result->getShortDescription());
-        $this->assertSame(self::arrayData()['image_url'], $result->getImageUrl());
-        $this->assertSame(self::arrayData()['unesco_site_url'], $result->getUnescoSiteUrl());
-        $this->assertSame(self::arrayData()['state_parties'], $result->getStatePartyCodes());
-        $this->assertSame(self::arrayData()['state_parties_meta'], $result->getStatePartiesMeta());
+        $this->assertSame($this->arrayData()['id'], $result->getId());
+        $this->assertSame($this->arrayData()['official_name'], $result->getOfficialName());
+        $this->assertSame($this->arrayData()['name'], $result->getName());
+        $this->assertSame($this->arrayData()['name_jp'], $result->getNameJp());
+        $this->assertSame($this->arrayData()['country'], $result->getCountry());
+        $this->assertSame($this->arrayData()['region'], $result->getRegion());
+        $this->assertSame($this->arrayData()['category'], $result->getCategory());
+        $this->assertSame($this->arrayData()['criteria'], $result->getCriteria());
+        $this->assertSame($this->arrayData()['year_inscribed'], $result->getYearInscribed());
+        $this->assertSame($this->arrayData()['area_hectares'], $result->getAreaHectares());
+        $this->assertSame($this->arrayData()['buffer_zone_hectares'], $result->getBufferZoneHectares());
+        $this->assertSame($this->arrayData()['is_endangered'], $result->isEndangered());
+        $this->assertSame($this->arrayData()['latitude'], $result->getLatitude());
+        $this->assertSame($this->arrayData()['longitude'], $result->getLongitude());
+        $this->assertSame($this->arrayData()['short_description'], $result->getShortDescription());
+        $this->assertSame($this->arrayData()['unesco_site_url'], $result->getUnescoSiteUrl());
+        $this->assertSame($this->arrayData()['state_parties'], $result->getStatePartyCodes());
+        $this->assertSame($this->arrayData()['state_parties_meta'], $result->getStatePartiesMeta());
+        $this->assertNotNull($result->getImages()[0]);
     }
 }
