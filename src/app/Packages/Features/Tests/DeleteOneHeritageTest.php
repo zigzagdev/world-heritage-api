bb8c1c0d30f9832a34e929ab81a87d2294fda3da
Application-drop-up <msts.oo0131@gmail.com>
2025-10-13 14:42:49 +0900

    fix: feature test method change

--- src/app/Packages/Features/Tests/DeleteOneHeritageTest.php
+++ src/app/Packages/Features/Tests/DeleteOneHeritageTest.php
@@ -2,11 +2,14 @@
 
 namespace App\Packages\Features\Tests;
 
+use App\Models\Image;
+use App\Packages\Domains\Infra\GcsImageObjectRemover;
 use Tests\TestCase;
 use Database\Seeders\DatabaseSeeder;
 use App\Models\Country;
 use App\Models\WorldHeritage;
 use Illuminate\Support\Facades\DB;
+use App\Packages\Domains\Ports\ObjectRemovePort;
 
 class DeleteOneHeritageTest extends TestCase
 {
@@ -17,6 +20,14 @@ class DeleteOneHeritageTest extends TestCase
         $this->refresh();
         $seeder = new DatabaseSeeder();
         $seeder->run();
+
+        $this->app->bind(ObjectRemovePort::class, function () {
+            return new class implements ObjectRemovePort {
+                public function remove(string $disk, string $key): void {}
+                public function removeByPrefix(string $disk, string $prefix): void {}
+            };
+        });
+
     }
 
     protected function tearDown(): void
@@ -32,6 +43,7 @@ class DeleteOneHeritageTest extends TestCase
              WorldHeritage::truncate();
              Country::truncate();
              DB::table('site_state_parties')->truncate();
+             Image::truncate();
              DB::connection('mysql')->statement('SET FOREIGN_KEY_CHECKS=1;');
         }
     }
@@ -41,11 +53,17 @@ class DeleteOneHeritageTest extends TestCase
         $result = $this->deleteJson(str_replace('{id}', '1418', $this->endpoint));
 
         $this->assertSame(200, $result->getStatusCode());
-        $this->assertDatabaseMissing('world_heritage_sites', [
+
+        $this->assertSoftDeleted('world_heritage_sites', [
             'id' => 1418,
         ]);
+
         $this->assertDatabaseMissing('site_state_parties', [
             'world_heritage_site_id' => 1418,
+        ]);
+
+        $this->assertSoftDeleted('images', [
+            'world_heritage_id' => 1418,
         ]);
     }
 }
