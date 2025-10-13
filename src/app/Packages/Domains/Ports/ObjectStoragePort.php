c68c9fc8aef11b046d72f5e229ef0c5799c1812e
Application-drop-up <msts.oo0131@gmail.com>
2025-10-13 12:16:39 +0900

    feat: refix new ports

--- src/app/Packages/Domains/Ports/ObjectStoragePort.php
+++ src/app/Packages/Domains/Ports/ObjectStoragePort.php
@@ -3,6 +3,6 @@ namespace App\Packages\Domains\Ports;
 
 interface ObjectStoragePort {
     public function put(string $disk, string $key, string $mime, $payload): void;
-    public function delete(string $disk, string $key): void;
+
     public function exists(string $disk, string $key): bool;
 }
