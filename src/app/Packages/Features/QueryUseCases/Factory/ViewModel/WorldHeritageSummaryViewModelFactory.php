c9bd5f122a51c0bbc3b63f73054b7d1a02ee771c
Application-drop-up <msts.oo0131@gmail.com>
2025-10-23 20:04:17 +0900

    feat: create new ViewModel & associated with its test file

---
+++ src/app/Packages/Features/QueryUseCases/Factory/ViewModel/WorldHeritageSummaryViewModelFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Packages\Features\QueryUseCases\Factory\ViewModel;
+
+use App\Packages\Features\QueryUseCases\Dto\WorldHeritageDto;
+
+class WorldHeritageSummaryViewModelFactory
+{
+    public static function build(WorldHeritageDto $dto): array
+    {
+        return [
+            'id' => $dto->getId(),
+            'official_name' => $dto->getOfficialName(),
+            'name' => $dto->getName(),
+            'name_jp' => $dto->getNameJp(),
+            'country' => $dto->getCountry(),
+            'region' => $dto->getRegion(),
+            'category' => $dto->getCategory(),
+            'criteria' => $dto->getCriteria(),
+            'year_inscribed' => $dto->getYearInscribed(),
+            'area_hectares' => $dto->getAreaHectares(),
+            'buffer_zone_hectares' => $dto->getBufferZoneHectares(),
+            'is_endangered' => $dto->isEndangered(),
+            'latitude' => $dto->getLatitude(),
+            'longitude' => $dto->getLongitude(),
+            'short_description' => $dto->getShortDescription(),
+            'unesco_site_url' => $dto->getUnescoSiteUrl(),
+            'state_party' => $dto->getStateParty(),
+            'state_party_codes' => $dto->getStatePartyCodes(),
+            'state_parties_meta' => $dto->getStatePartiesMeta(),
+            'thumbnail_url' => $dto->getThumbnailUrl(),
+        ];
+    }
+}
