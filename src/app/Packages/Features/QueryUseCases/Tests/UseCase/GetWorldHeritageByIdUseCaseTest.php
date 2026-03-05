40bd9a25f9b5c3bd3a4bdddce0d70f3c39ea118f
Application-drop-up <msts.oo0131@gmail.com>
2026-03-05 22:34:31 +0900

    fix: failed test in Application Layer UseCase

--- src/app/Packages/Features/QueryUseCases/Tests/UseCase/GetWorldHeritageByIdUseCaseTest.php
+++ src/app/Packages/Features/QueryUseCases/Tests/UseCase/GetWorldHeritageByIdUseCaseTest.php
@@ -7,6 +7,7 @@ use App\Models\WorldHeritage;
 use App\Models\Image;
 use App\Packages\Domains\WorldHeritageQueryService;
 use App\Packages\Features\QueryUseCases\Dto\WorldHeritageDto;
+use App\Packages\Features\QueryUseCases\QueryServiceInterface\WorldHeritageQueryServiceInterface;
 use App\Packages\Features\QueryUseCases\UseCase\GetWorldHeritageByIdUseCase;
 use Database\Seeders\DatabaseSeeder;
 use Illuminate\Support\Facades\DB;
@@ -16,13 +17,10 @@ use Mockery;
 
 class GetWorldHeritageByIdUseCaseTest extends TestCase
 {
-    private $queryService;
     protected function setUp(): void
     {
         parent::setUp();
         $this->refresh();
-
-        $this->queryService = app(WorldHeritageQueryService::class);
 
         $seeder = new DatabaseSeeder();
         $seeder->run();
@@ -46,13 +44,47 @@ class GetWorldHeritageByIdUseCaseTest extends TestCase
         }
     }
 
+    private function mockQueryService(): WorldHeritageQueryServiceInterface
+    {
+        $queryService = Mockery::mock(WorldHeritageQueryServiceInterface::class);
+
+        $queryService
+            ->shouldReceive('getHeritageById')
+            ->andReturn(new WorldHeritageDto(
+                self::arrayData()['id'],
+                self::arrayData()['official_name'],
+                self::arrayData()['name'],
+                self::arrayData()['country'],
+                self::arrayData()['country_name_jp'] ?? null,
+                self::arrayData()['region'],
+                self::arrayData()['category'],
+                self::arrayData()['year_inscribed'],
+                self::arrayData()['latitude'],
+                self::arrayData()['longitude'],
+                self::arrayData()['is_endangered'],
+                self::arrayData()['heritage_name_jp'] ?? null,
+                self::arrayData()['state_party'],
+                self::arrayData()['criteria'],
+                self::arrayData()['area_hectares'],
+                self::arrayData()['buffer_zone_hectares'],
+                self::arrayData()['short_description'],
+                null,
+                null,
+                self::arrayData()['unesco_site_url'],
+                self::arrayData()['state_party_codes'],
+                self::arrayData()['state_parties_meta'],
+            ));
+
+        return $queryService;
+    }
+
     private static function arrayData(): array
     {
         return [
             'id' => 1133,
-            'official_name' => "Ancient and Primeval Beech Forests of the Carpathians and Other Regions of Europe",
-            'name' => "Ancient and Primeval Beech Forests",
-            'name_jp' => "カルパティア山脈とヨーロッパ各地の古代及び原生ブナ林",
+            'official_name' => 'Ancient and Primeval Beech Forests of the Carpathians and Other Regions of Europe',
+            'name' => 'Ancient and Primeval Beech Forests',
+            'heritage_name_jp' => 'カルパティア山脈とヨーロッパ各地の古代及び原生ブナ林',
             'country' => 'Slovakia',
             'region' => 'Europe',
             'category' => 'Natural',
@@ -65,37 +97,53 @@ class GetWorldHeritageByIdUseCaseTest extends TestCase
             'latitude' => 0.0,
             'longitude' => 0.0,
             'short_description' => '氷期後のブナの自然拡散史を示すヨーロッパ各地の原生的ブナ林群から成る越境・連続資産。',
-            'image_url' => '',
             'unesco_site_url' => 'https://whc.unesco.org/en/list/1133',
-            'state_parties_codes' => [
-                'ALB','AUT','BEL','BIH','BGR','HRV','CZE','FRA','DEU','ITA','MKD','POL','ROU','SVK','SVN','ESP','CHE','UKR'
+            'state_party_codes' => [
+                'ALB',
+                'AUT',
+                'BEL',
+                'BIH',
+                'BGR',
+                'HRV',
+                'CZE',
+                'FRA',
+                'DEU',
+                'ITA',
+                'MKD',
+                'POL',
+                'ROU',
+                'SVK',
+                'SVN',
+                'ESP',
+                'CHE',
+                'UKR',
             ],
             'state_parties_meta' => [
-                'ALB' => ['is_primary' => false, 'inscription_year' => 2007],
-                'AUT' => ['is_primary' => false, 'inscription_year' => 2007],
-                'BEL' => ['is_primary' => false, 'inscription_year' => 2007],
-                'BIH' => ['is_primary' => false, 'inscription_year' => 2007],
-                'BGR' => ['is_primary' => false, 'inscription_year' => 2007],
-                'HRV' => ['is_primary' => false, 'inscription_year' => 2007],
-                'CZE' => ['is_primary' => false, 'inscription_year' => 2007],
-                'FRA' => ['is_primary' => false, 'inscription_year' => 2007],
-                'DEU' => ['is_primary' => false, 'inscription_year' => 2007],
-                'ITA' => ['is_primary' => false, 'inscription_year' => 2007],
-                'MKD' => ['is_primary' => false, 'inscription_year' => 2007],
-                'POL' => ['is_primary' => false, 'inscription_year' => 2007],
-                'ROU' => ['is_primary' => false, 'inscription_year' => 2007],
-                'SVK' => ['is_primary' => true,  'inscription_year' => 2007],
-                'SVN' => ['is_primary' => false, 'inscription_year' => 2007],
-                'ESP' => ['is_primary' => false, 'inscription_year' => 2007],
-                'CHE' => ['is_primary' => false, 'inscription_year' => 2007],
-                'UKR' => ['is_primary' => false, 'inscription_year' => 2007],
-            ]
+                'ALB' => ['is_primary' => false],
+                'AUT' => ['is_primary' => false],
+                'BEL' => ['is_primary' => false],
+                'BIH' => ['is_primary' => false],
+                'BGR' => ['is_primary' => false],
+                'HRV' => ['is_primary' => false],
+                'CZE' => ['is_primary' => false],
+                'FRA' => ['is_primary' => false],
+                'DEU' => ['is_primary' => false],
+                'ITA' => ['is_primary' => false],
+                'MKD' => ['is_primary' => false],
+                'POL' => ['is_primary' => false],
+                'ROU' => ['is_primary' => false],
+                'SVK' => ['is_primary' => true],
+                'SVN' => ['is_primary' => false],
+                'ESP' => ['is_primary' => false],
+                'CHE' => ['is_primary' => false],
+                'UKR' => ['is_primary' => false],
+            ],
         ];
     }
 
     public function test_use_case(): void
     {
-        $useCase = new GetWorldHeritageByIdUseCase($this->queryService);
+        $useCase = new GetWorldHeritageByIdUseCase($this->mockQueryService());
 
         $result = $useCase->handle(self::arrayData()['id']);
 
