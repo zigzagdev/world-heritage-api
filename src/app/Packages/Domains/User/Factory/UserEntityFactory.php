09ed61208299bda244ffd449ed481ad1845a0bde
Application-drop-up <msts.oo0131@gmail.com>
2026-06-27 21:23:31 +0900

    feat: implement UserEntityFactory

--- src/app/Packages/Domains/User/Factory/UserEntityFactory.php
+++ src/app/Packages/Domains/User/Factory/UserEntityFactory.php
@@ -1,20 +1,20 @@
 <?php
 
-namespace App\Packages\Domains\User;
+namespace App\Packages\Domains\User\Factory;
 
-use App\Packages\Domains\User\Subscription\Subscription;
-use App\Packages\Domains\User\Subscription\SubscriptionTier;
-use DateTimeImmutable;
+use App\Packages\Domains\User\Subscription\SubscriptionFactory;
+use App\Packages\Domains\User\UserEntity;
+use App\Packages\Domains\User\AgeRange;
 
 final class UserEntityFactory
 {
     public static function build(array $data): UserEntity
     {
-        $subscription = new Subscription(
-            SubscriptionTier::from($data['subscription_tier']),
-            $data['subscription_expires_at'] ?? null,
-            $data['now'] ?? null,
-        );
+        $subscription = SubscriptionFactory::build([
+            'tier' => $data['subscription_tier'],
+            'expires_at' => $data['subscription_expires_at'] ?? null,
+            'now' => $data['now'] ?? null,
+        ]);
 
         return new UserEntity(
             $data['id'],
