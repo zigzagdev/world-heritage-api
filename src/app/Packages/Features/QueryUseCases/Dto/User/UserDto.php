5ac254394df596585263a99ff2640fd4b09f0346
Application-drop-up <msts.oo0131@gmail.com>
2026-06-28 18:07:49 +0900

    feat: update UserDto with typed public readonly properties

--- src/app/Packages/Features/QueryUseCases/Dto/User/UserDto.php
+++ src/app/Packages/Features/QueryUseCases/Dto/User/UserDto.php
@@ -2,7 +2,15 @@
 
 namespace App\Packages\Features\QueryUseCases\Dto\User;
 
-class UserDto
+final class UserDto
 {
-
+    public function __construct(
+        public readonly int     $id,
+        public readonly string  $firstName,
+        public readonly string  $lastName,
+        public readonly string  $email,
+        public readonly string  $ageRange,
+        public readonly string  $subscriptionTier,
+        public readonly ?string $subscriptionExpiresAt,
+    ) {}
 }
