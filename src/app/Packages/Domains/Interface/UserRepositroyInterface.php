0a1611c47bc56acb4e899e87e04cf2431d2bfcc9
Application-drop-up <msts.oo0131@gmail.com>
2026-06-29 09:13:48 +0900

    feat: add updateUser to UserRepositroyInterface and UserRepository

--- src/app/Packages/Domains/Interface/UserRepositroyInterface.php
+++ src/app/Packages/Domains/Interface/UserRepositroyInterface.php
@@ -8,4 +8,6 @@ use App\Packages\Features\QueryUseCases\Dto\User\UserDto;
 interface UserRepositroyInterface
 {
     public function createUser(UserEntity $entity): UserDto;
+
+    public function updateUser(UserEntity $entity): UserDto;
 }
