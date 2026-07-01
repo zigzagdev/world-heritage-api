f9548291391d948f4184d4a63eb77b0f88b38534
Application-drop-up <msts.oo0131@gmail.com>
2026-07-02 08:03:04 +0900

    feat: implement UpdateUserUseCase

---
+++ src/app/Packages/Features/CommandUseCases/UseCase/User/UpdateUserUseCase.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Packages\Features\CommandUseCases\UseCase\User;
+
+use App\Packages\Domains\Interface\UserRepositroyInterface;
+use App\Packages\Domains\User\Factory\UserEntityFactory;
+use App\Packages\Features\CommandUseCases\UseCommand\User\UpdateUserCommand;
+use App\Packages\Features\QueryUseCases\Dto\User\UserDto;
+
+final class UpdateUserUseCase
+{
+    public function __construct(
+        private readonly UserRepositroyInterface $userRepository,
+    ) {}
+
+    public function handle(UpdateUserCommand $command): UserDto
+    {
+        $entity = UserEntityFactory::build([
+            'id'                      => $command->id,
+            'first_name'              => $command->firstName,
+            'last_name'               => $command->lastName,
+            'email'                   => $command->email,
+            'age_range'               => $command->ageRange,
+            'subscription_tier'       => $command->subscriptionTier,
+            'subscription_expires_at' => $command->subscriptionExpiresAt,
+        ]);
+
+        return $this->userRepository->updateUser($entity);
+    }
+}
