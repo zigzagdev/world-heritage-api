1579f48ce17eba8ed5d4f77f487ae58e34fa3a40
Application-drop-up <msts.oo0131@gmail.com>
2026-09-07 09:30:12 +0900

    fix: cast is_transboundary to boolean on WorldHeritage model

--- src/app/Models/WorldHeritage.php
+++ src/app/Models/WorldHeritage.php
@@ -72,6 +72,7 @@ class WorldHeritage extends Model
         return [
             'criteria' => 'array',
             'is_endangered'  => 'boolean',
+            'is_transboundary' => 'boolean',
             'year_inscribed' => 'integer',
             'area_hectares'  => 'float',
             'buffer_zone_hectares' => 'float',
